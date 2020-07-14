5d535d51bf8734a9ff5b1a721a34f2f7a479e6cb
u-ahmedofficial <umairahmedofficial1@gmail.com>
2020-07-15 04:36:36 +0500

    Add Important Libraries

---
+++ Encrypter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+################################################
+
+
+# Some important Libraries to be added
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+import time
+import shutil
+import sqlite3
+import hashlib
+from sys import platform
+
+
